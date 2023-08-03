932ec450fb769a5fc7dbc3f57a1cef7411ebe10d
Logan Garrett <ninetailsoftware@gmail.com>
2023-08-02 23:52:12 -0400

    Trying to understand insertionSort.

---
+++ insertionSort.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void insertionSort() {
+    int array[] = {3, 5, 9, 7, 10, 13, 1};
+    int i = 0;
+    
+    printf("Start");
+
+    while (i <= 6) {
+        printf(" -> ");
+        printf("%d", array[i]);
+        i++;
+    }
+}
+
+int main() {
+    printf("-----------------\n");
+    insertionSort();
+    return 0;
+}
