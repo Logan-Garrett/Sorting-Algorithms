a1ca99f36e369cbe1b1421df4907c7eb9fcc4b75
NineTailSecurity <ninetailsoftware@gmail.com>
2023-08-22 23:35:01 -0400

    Added extra

--- insertionSort.c
+++ insertionSort.c
@@ -3,13 +3,13 @@
 
 
 void insertionSort() {
-    	int array[] = {3, 5, 9, 7, 1, 4, 8};
+    	int array[] = {14, 3, 5, 9, 7, 1, 4, 10, 8};
     	int i = 0;
     	int j = i + 1;
 
     	printf("Start ");
 
-    	while (i < 7) {
+    	while (i < 9) {
 		while (array[j] < array[i]) {
 			int tmp = array[i];
 			array[i] = array[j];
@@ -21,7 +21,7 @@ void insertionSort() {
 		i++;
 		j = i + 1;
     	}
-	printf("%d %d %d %d %d %d %d\n", array[0], array[1], array[2], array[3], array[4], array[5], array[6]);
+	printf("%d %d %d %d %d %d %d %d %d\n", array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8]);
 }
 
 int main() {
